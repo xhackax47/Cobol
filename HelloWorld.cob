ef5064b99d92036ec21714d629a7065155eda40c
Samy Chaabi <xhackax47@gmail.com>
2022-03-31 00:31:06 +0200

    Add Free Format Code

---
+++ HelloWorld.cob
@@ -0,0 +1,6 @@
+      *HelloWorld COBOL
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. hello.
+       PROCEDURE DIVISION.
+       DISPLAY 'Hello world by xhackax47 in COBOL !!!'.
+       STOP RUN.
---
+++ hellonew.cob
@@ -0,0 +1,8 @@
+*> Sample GnuCOBOL program
+identification division.
+program-id. hellonew.
+procedure division.
+display
+"Hello, it's new free format code COBOL !"
+end-display
+goback.
