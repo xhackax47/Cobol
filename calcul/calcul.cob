a085680761f1db7d93173cc19697f52f455f9022
Samy Chaabi <xhackax47@gmail.com>
2022-03-31 01:28:13 +0200

    Ajout de commentaires

--- calcul/calcul.cob
+++ calcul/calcul.cob
@@ -2,25 +2,26 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. ExempleEtiquette.
 
+      * Division et sections des variables
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        77 a PIC 9(15).
 
-
+      * pla = Plage d'affichage & pls = Plage de saisie
        SCREEN SECTION.
-       1 pls-exemple.
+       1 pls.
            2 BLANK SCREEN.
            2 LINE 6 COL 10 VALUE 'Saisir une valeur : '.
            2 saisie PIC z(15) TO a REQUIRED.
  
-       1 pla-exemple.
+       1 pla.
            2 LINE 8 COL 10 VALUE 'Resultat : '.
            2 COL 30 PIC z(15) FROM saisie.
 
        PROCEDURE DIVISION.
  
-       DISPLAY pls-exemple.
+       DISPLAY pls.
        ACCEPT saisie.
-       DISPLAY pla-exemple.
+       DISPLAY pla.
 
        STOP RUN.
