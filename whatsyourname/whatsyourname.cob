c2c4fdd05e8b044d14d3867b4b06aa6c0e6d94e2
Samy Chaabi <xhackax47@gmail.com>
2022-03-31 01:25:07 +0200

    Ajout de commentaires

--- whatsyourname/whatsyourname.cob
+++ whatsyourname/whatsyourname.cob
@@ -1,34 +1,36 @@
       * Exemple code structuré
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. HelloWorld.
+      * Nom du programme
+       PROGRAM-ID. WhatsYourName.
            
+      * Division et sections des variables
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        77 nom PIC x(25).
  
-
+      * plg-a = Plage d'affichage & plg-s = Plage de saisie
        SCREEN SECTION.
-       1 a-plg-titre.
+       1 plg-a-titre.
            2 BLANK SCREEN.
            2 LINE 6 COL 10 VALUE 'Hey !'.
  
-       1 s-plg-nom.
+       1 plg-s-nom.
            2 LINE 8 COL 8 VALUE 'Quel est ton nom ? '.
            2 PIC x(25) TO nom REQUIRED.
  
-       1 a-plg-nom.
+       1 plg-a-nom.
            2 LINE 10 COL 8 VALUE 'Salut'.
            2 COL 15 PIC x(25) FROM nom.
  
        PROCEDURE DIVISION.
  
       * On affiche le nom de notre programme
-       DISPLAY a-plg-titre. 
+       DISPLAY plg-a-titre. 
  
       * Ensuite on affiche le formulaire de saisie
-       DISPLAY s-plg-nom. 
-       ACCEPT s-plg-nom.  
-       DISPLAY a-plg-nom. 
+       DISPLAY plg-s-nom. 
+       ACCEPT plg-s-nom.  
+       DISPLAY plg-a-nom. 
   
        STOP RUN.
        
