794dcfa4869fa2fbf84feed250cb6f3dc62154dd
Samy Chaabi <xhackax47@gmail.com>
2022-03-31 00:11:56 +0200

    Init COBOL

---
+++ HelloWorld.cbl
@@ -0,0 +1,13 @@
+identification division.
+program-id. ProgrammeTutoCobol1.
+
+SCREEN SECTION.
+1 pla.
+       2 BLANK SCREEN.
+       2 LINE 6 COL 10 VALUE 'Hello world by xhackax47 in COBOL !!!'.
+       
+procedure division.
+
+display pla.
+
+stop run.
